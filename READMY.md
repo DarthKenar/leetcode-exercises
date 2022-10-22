feb44ba017d2a51247c376fc6ac1fceae18a2d4e
DarthKenar <federico_vega22@hotmail.com>
2022-10-22 15:05:28 -0300

    Finished exercise (Two sum), added READMY.MD

---
+++ READMY.md
@@ -0,0 +1,5 @@
+This repository show solutions to leetcode web exercises made in python
+
+look at my codes :D
+
+https://leetcode.com/DarthKenar/
--- two-sum/two-sum.py
+++ two-sum/two-sum.py
@@ -1,2 +1,9 @@
+from typing import List
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
+        
+        for index_first in range(0,len(nums)):
+            for index_second in range(0,len(nums)):
+                
+                if nums[index_first] + nums[index_second] == target:
+                    return [index_first, index_second]
