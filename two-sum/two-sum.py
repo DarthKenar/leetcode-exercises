c51d347987fb2ca048b7d57a4d5eef57e681169e
DarthKenar <federico_vega22@hotmail.com>
2022-10-22 16:04:49 -0300

    Solution found

--- two-sum/two-sum.py
+++ two-sum/two-sum.py
@@ -1,11 +1,13 @@
 from typing import List
+
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        
-        for index_first in range(0,len(nums)):
-            for index_second in range(0,len(nums)):
+
+        for index in range(0,len(nums)):
+
+            transfer = target - nums[index]
+            nums[index] = None
+
+            if transfer in nums:
                 
-                if nums[index_first] != nums[index_second]:
-                    if nums[index_first] + nums[index_second] == target:
-                        return [index_first, index_second]
-                    
+                return [index, nums.index(transfer)]
