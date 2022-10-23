c6622c0ab9594780f8c899623660408cc7094b60
DarthKenar <federico_vega22@hotmail.com>
2022-10-23 02:36:02 -0300

    Exercise finished - First Letter to Appear Twice - Submit OK

--- First-Letter-to-Appear-Twice/FLtAT.py
+++ First-Letter-to-Appear-Twice/FLtAT.py
@@ -1,20 +1,7 @@
 class Solution:
     def repeatedCharacter(self, s: str) -> str:
 
-        length_checkout = 0
-        iteration_counter = len(s)
-
-        
-        def checkout(s,length_checkout, iteration_counter):
-
-            length_checkout += 1
-            iteration_counter -= 1
-
-            for i in range(0,iteration_counter):
-                
-                if s[i] == s[i+length_checkout]:
+        for i in range(1, len(s)):
+            for j in range(i-1, 0, -1):
+                if s[i] == s[j]:
                     return s[i]
-
-            return(checkout(s,length_checkout,iteration_counter))
-
-        return(checkout(s,length_checkout,iteration_counter))
