ca28694acf72d6d22fca45c0fa779bcb6e3f6b63
DarthKenar <federico_vega22@hotmail.com>
2022-10-25 00:31:54 -0300

    solution found for exercise 'Median of two sorted arrays', I need to restructure the algorithm to make it more efficient.

--- Median-of-Two-Sorted-Arrays/MOTSA.py
+++ Median-of-Two-Sorted-Arrays/MOTSA.py
@@ -2,26 +2,35 @@ class Solution:
     def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
         
         def comprobacion(longitud, lista):
-            #es par
-            if longitud % 2 == 0: 
+            
+            if longitud == 1:
                 
-                return ((lista[longitud/2]+lista[(longitud/2)+1])/2)
+                return(lista[0])
+            
+            elif longitud % 2 == 0: 
                 
-            #es impar
+                return ((lista[int((longitud/2)-1)]+lista[int(longitud/2)])/2)
+            
             else:
                 
-                return (lista[longitud//2])
+                return (lista[int(longitud//2)])
         
         len_nums1 = len(nums1)
         len_nums2 = len(nums2)
         
         if len_nums1 > 0 and len_nums2 == 0:
             
-            comprobacion(len_nums1, nums1)
+            return(comprobacion(len_nums1, nums1))
         
         elif len_nums2 > 0 and len_nums1 == 0:
             
-            comprobacion(len_nums2,nums2)
+            return(comprobacion(len_nums2,nums2))
         
         else:
-            return ((comprobacion(len_nums1, nums1)+comprobacion(len_nums2,nums2)/2))
+            
+            for i in range(0, len_nums2):
+                
+                nums1.append(nums2[i])
+                nums1.sort()
+                
+            return comprobacion((len_nums1+len_nums2), nums1)
