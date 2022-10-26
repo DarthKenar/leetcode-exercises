f76f51280335e8a6087ae2c7d9e1948bade9bd8d
Federico HP <federico_vega22@hotmail.com>
2022-10-26 15:07:26 -0300

    Solution found, i need to find a better solution

--- Longest-Common-Prefix/LCP.py
+++ Longest-Common-Prefix/LCP.py
@@ -1,16 +1,29 @@
-strs = ["flrt","flow","flight"]
-respuesta = ""
-length = (len(strs))
-#print(f"{len(strs[0]) =}")
-if length == 1:
-    print("")
-for j in range(0,(len(strs[0]))-1):
-    counter = 0
-    for i in range(1,length):
-        if strs[0][j] == strs[i][j]:
-            counter +=1
-    if counter == length-1:
-        respuesta = respuesta + strs[0][j]
-    else:
-        print(respuesta)
-        break
+from typing import List
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        respuesta = ""
+        length = (len(strs))
+        if length == 1:
+            return("")
+        #este for si es 6 va de 0 a 5
+        #esta perfecto
+        for j in range(0,(len(strs[0]))):
+
+            counter = 0
+            #bien el for
+            for i in range(1,length):
+
+                if j < len(strs[i]):
+                    if strs[0][j] == strs[i][j]:
+                        counter +=1
+
+            if counter == length-1:
+                respuesta = respuesta + strs[0][j]
+            else:
+                return(respuesta)
+                break
+        return(respuesta)
+
+lista = ["f","flo","flight"]
+solucion = Solution()
+print(solucion.longestCommonPrefix(lista))
