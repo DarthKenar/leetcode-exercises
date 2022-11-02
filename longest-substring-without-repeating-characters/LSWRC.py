d2dfbebc17dcf80818e63891ec992510ccfaa6ba
Federico HP <federico_vega22@hotmail.com>
2022-11-02 14:20:56 -0300

    Finish exersice - longest substring without repeating characters

--- longest-substring-without-repeating-characters/LSWRC.py
+++ longest-substring-without-repeating-characters/LSWRC.py
@@ -1,25 +1,28 @@
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
-        
-        #In case empty s
+
         if len(s) == 0: return 0
 
         substring = s[0]
         output = 0
         list_string = [s[0]]
+        s = s + s[-1]
 
         for i in range(1,len(s)):
-            if s[i] not in substring:
-                substring = substring + s[i]
-            else:
-                list_string.append(substring)
-                substring = s[i]
 
-        #if len(list_string) == 1: return len(s)
+            if s[i] not in substring:
+
+                substring = substring + s[i]
+
+            else:
+
+                list_string.append(substring)
+                substring = substring[substring.index(s[i])+1:] + s[i]
 
         for i in list_string:
+
             if len(i) > output:
+
                 output = len(i)
+                
         return(output)
-a = Solution()
-print(a.lengthOfLongestSubstring("aab"))
