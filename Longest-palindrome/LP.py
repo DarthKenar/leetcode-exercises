e3ccd11bf15c3ff9e75a359b3d8210d5aa352149
Federico HP <federico_vega22@hotmail.com>
2022-11-04 15:14:25 -0300

    Find second solution for longest Palindrome substring, i can do better

--- Longest-palindrome/LP.py
+++ Longest-palindrome/LP.py
@@ -2,17 +2,24 @@ class Solution:
     """"a better way to do this is revert the implemented system
     if check first the longest word instead of the smallest
     """
+    #"HolaMundo"
     def longestPalindrome(self, s: str) -> str:
-        palindrome = ""
-        for i in range(len(s)):
-            for j in range(len(s)):
-                if s[0+j:2+j+i] == (s[0+j:2+j+i])[::-1]:
-                    if len(palindrome) < len(s[0+j:2+j+i]):
-                        palindrome = s[0+j:2+j+i]
-                        
-        return palindrome
+
+        lenght = len(s) #"HolaMunud"
+        if s == s[::-1]: return s
+        check = 1
+        
+        while True:
+            
+            for i in range(0,check):
+                if s[i:lenght+i-check] == s[i:lenght+i-check][::-1]:
+                    return s[i:lenght+i-check]
+                    break
+            check += 1
+
 
 
 a = Solution()
-a.longestPalindrome("babad")
+print(a.longestPalindrome("abb"))
+
 
