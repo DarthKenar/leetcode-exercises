02500eb5fdb03de71092cb2980fceed248231822
Federico Vega - HP <federico_Vega22@hotmail.com>
2022-11-25 14:38:56 -0300

    finished exercise 35

--- 35-Search-Insert-Position/SIP.py
+++ 35-Search-Insert-Position/SIP.py
@@ -2,4 +2,8 @@ from typing import List
 class Solution:
     def searchInsert(self, nums: List[int], target: int) -> int:
         if target in nums:
-            return nums.
+            return nums.index(target)
+        else:
+            nums.append(target)
+            nums.sort()
+            return nums.index(target)
