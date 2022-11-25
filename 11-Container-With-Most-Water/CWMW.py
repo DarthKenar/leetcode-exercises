1af8a3d068942b9c2d9b65495ba30dd6818202d9
Federico Vega - HP <federico_Vega22@hotmail.com>
2022-11-25 14:06:58 -0300

    finished exercise 11, container whit most water

--- 11-Container-With-Most-Water/CWMW.py
+++ 11-Container-With-Most-Water/CWMW.py
@@ -1,30 +1,15 @@
+from typing import List
 class Solution:
     def maxArea(self, height: List[int]) -> int:
-        
-        reverse = height.copy()
-        reverse.sort(reverse=True)
-        output = 0
-
-        for i in range(1,len(reverse)):
-
-            index_one = height.index(reverse[i-1])
-            
-            try:
-                
-                index_two = len(height[:index_one]) + height[index_one+1:].index(reverse[i]) +1
-                
-            except:
-                
-                index_two = height[:index_one].index(reverse[i])
-
-            lower = min(height[index_one],height[index_two])
-            width = max(index_one, index_two) - min(index_one, index_two)
-            max_water = width*lower
-            
-            if max_water > output:
-                
-                output = max_water
-                
-        return output
-
-
+        maxim = 0 
+        i = 0
+        j = len(height)-1
+        while i < j:
+            width = abs(i-j)
+            area = width * min(height[i],height[j])
+            maxim = max(area,maxim)
+            if height[i] > height[j]:
+                j -=1
+            else:
+                i +=1
+        return maxim 
