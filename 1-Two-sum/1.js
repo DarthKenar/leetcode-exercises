1662f22b6a9976f6c3496caeddb2d7d17e0a30e8
Federico <federico_vega22@hotmail.com>
2023-11-08 20:15:21 -0300

    linked list added for practice

---
+++ 1-Two-sum/1.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+    for (let index = 0; index < nums.length; index++) {
+        const element = array[index];
+        
+    }
+};
+
+// class Solution:
+//     def twoSum(self, nums: List[int], target: int) -> List[int]:
+
+//         for index in range(0,len(nums)):
+
+//             transfer = target - nums[index]
+//             nums[index] = None
+
+//             if transfer in nums:
+                
+//                 return [index, nums.index(transfer)]
